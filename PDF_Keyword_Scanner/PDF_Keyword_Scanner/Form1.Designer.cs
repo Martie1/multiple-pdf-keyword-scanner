55d28a96d41443d9c042f57b1bbb9334bbf34692
Martie <martiinamay@gmail.com>
2024-08-09 18:23:20 +0200

    1.0 PDF keywords scanner

---
+++ PDF_Keyword_Scanner/PDF_Keyword_Scanner/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace CV_Scanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            labelChoose = new Label();
+            btnBrowse = new Button();
+            textDirectory = new TextBox();
+            textKeywords = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            btnApply = new Button();
+            checkBox = new CheckBox();
+            SuspendLayout();
+            // 
+            // labelChoose
+            // 
+            labelChoose.AutoSize = true;
+            labelChoose.Location = new Point(38, 52);
+            labelChoose.Name = "labelChoose";
+            labelChoose.Size = new Size(180, 15);
+            labelChoose.TabIndex = 0;
+            labelChoose.Text = "Choose the location of directory.";
+            labelChoose.TextAlign = ContentAlignment.BottomLeft;
+            labelChoose.Click += label1_Click;
+            // 
+            // btnBrowse
+            // 
+            btnBrowse.Location = new Point(475, 75);
+            btnBrowse.Name = "btnBrowse";
+            btnBrowse.Size = new Size(75, 23);
+            btnBrowse.TabIndex = 1;
+            btnBrowse.Text = "Browse...";
+            btnBrowse.UseVisualStyleBackColor = true;
+            btnBrowse.Click += btnBrowse_Click;
+            // 
+            // textDirectory
+            // 
+            textDirectory.Location = new Point(38, 76);
+            textDirectory.Name = "textDirectory";
+            textDirectory.Size = new Size(431, 23);
+            textDirectory.TabIndex = 2;
+            // 
+            // textKeywords
+            // 
+            textKeywords.Location = new Point(38, 143);
+            textKeywords.Name = "textKeywords";
+            textKeywords.Size = new Size(431, 23);
+            textKeywords.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(38, 125);
+            label1.Name = "label1";
+            label1.Size = new Size(87, 15);
+            label1.TabIndex = 6;
+            label1.Text = "Enter keywords";
+            label1.TextAlign = ContentAlignment.BottomLeft;
+            label1.Click += label1_Click_2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(38, 169);
+            label2.Name = "label2";
+            label2.Size = new Size(499, 15);
+            label2.TabIndex = 7;
+            label2.Text = "*Please use one word keywords and ',' as a separator. Size of letters will not impact the search ";
+            label2.TextAlign = ContentAlignment.BottomLeft;
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(38, 353);
+            label3.Name = "label3";
+            label3.Size = new Size(370, 15);
+            label3.TabIndex = 8;
+            label3.Text = "*By default the program finds files with ANY of the entered keywords";
+            label3.TextAlign = ContentAlignment.MiddleRight;
+            label3.Click += label3_Click;
+            // 
+            // btnApply
+            // 
+            btnApply.Location = new Point(575, 346);
+            btnApply.Name = "btnApply";
+            btnApply.Size = new Size(90, 29);
+            btnApply.TabIndex = 9;
+            btnApply.Text = "Apply";
+            btnApply.UseVisualStyleBackColor = true;
+            btnApply.Click += btnApply_Click;
+            // 
+            // checkBox
+            // 
+            checkBox.AutoSize = true;
+            checkBox.Location = new Point(38, 331);
+            checkBox.Name = "checkBox";
+            checkBox.Size = new Size(268, 19);
+            checkBox.TabIndex = 10;
+            checkBox.Text = "Find the files containing ALL of the keywords*";
+            checkBox.UseVisualStyleBackColor = true;
+            checkBox.CheckedChanged += checkBox_CheckedChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(687, 401);
+            Controls.Add(checkBox);
+            Controls.Add(btnApply);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textKeywords);
+            Controls.Add(textDirectory);
+            Controls.Add(btnBrowse);
+            Controls.Add(labelChoose);
+            Name = "Form1";
+            Text = "CV Keywords Manager";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label labelChoose;
+        private Button btnBrowse;
+        private TextBox textDirectory;
+        private TextBox textKeywords;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button btnApply;
+        private CheckBox checkBox;
+    }
+}
---
+++ PDF_Keyword_Scanner/PDF_Keyword_Scanner/Form1.cs
@@ -0,0 +1,260 @@
+
+using System.Collections.Immutable;
+using System.Runtime;
+using System.Runtime.InteropServices;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+using UglyToad.PdfPig.Graphics.Operations.PathPainting;
+
+namespace CV_Scanner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //clicking on the label as well as the browse button will do the same thing
+        private void label1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            DialogResult result = folder.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textDirectory.Text = folder.SelectedPath;
+            }
+        }
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            DialogResult result = folder.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textDirectory.Text = folder.SelectedPath;
+            }
+        }
+        private void btnApply_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = textDirectory.Text; //selected dir for scanning
+                string[] keywords = textKeywords.Text.Split(',').Select(k => k.Replace(" ", "")).Distinct().ToArray();
+                Array.Sort(keywords);
+                //creating new folder inside of path
+                bool all = checkBox.Checked;
+                //gets the value of checkbox
+                string dirName = "";
+                if (all) //all keywords(and) true, any keywords(or) false, 
+                {
+                    dirName = "PDF_allOf_" + string.Join("_", keywords);
+                }
+                else
+                {
+                    dirName = "PDF_anyOf_" + string.Join("_", keywords);
+                }
+                string newDirPath = Path.Combine(path, dirName);
+
+
+
+                var pdfFiles = Directory.GetFiles(path, "*.pdf"); //could implement handling other formats later
+
+
+                var count = 0;
+                //	var fun = any ? ContainsAnyKeywords : ContainsAllKeywords;
+                if (!all)
+                {
+                    string newPath = Path.Combine("ANY", newDirPath);
+                    if (!Directory.Exists(newPath))
+                    {
+                        Directory.CreateDirectory(newPath);
+                    }
+
+                    foreach (var pdfFile in pdfFiles)
+                    {
+                        if (ContainsAnyKeywords(pdfFile, keywords))
+                        {
+                            count++;
+                            string fileName = Path.GetFileName(pdfFile);
+                            string destFilePath = Path.Combine(newDirPath, fileName);
+                            File.Copy(pdfFile, destFilePath, true);
+                        }
+                    }
+                }
+                else if (all)
+                {
+                    string newPath = Path.Combine("ALL", newDirPath);
+                    if (!Directory.Exists(newPath))
+                    {
+                        Directory.CreateDirectory(newPath);
+                    }
+                    foreach (var pdfFile in pdfFiles)
+                    {
+                        if (ContainsAllKeywords(pdfFile, keywords))
+                        {
+
+                            string fileName = Path.GetFileName(pdfFile);
+                            string destFilePath = Path.Combine(newDirPath, fileName);
+                            File.Copy(pdfFile, destFilePath, true);
+                        }
+                    }
+                   
+                }
+               // string finalMessage = "Scanning complete : "+count.ToString()+" matches found";
+                string finalMessage = $"Scanning complete : {count} matches found"; //fancy way
+
+                if (!Directory.EnumerateFileSystemEntries(path).Any()) //if the dir is empty (no matches found)
+                {
+                    finalMessage = "No matches found.";
+                    Directory.Delete(path);
+                }
+                ShowCompletionDialog(newDirPath, count,finalMessage);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occured: {ex.Message}");
+            }
+        }
+
+        //have to include if the keywords dont match anything
+        private void ShowCompletionDialog(string folderPath, int count,string finalMessage)
+        {
+            Form dialog = new Form
+            {
+                Width = 300,
+                Height = 150,
+                Text = "Scanning Complete"
+            };
+
+            Label label = new Label
+            {
+                Left = 50,
+                Top = 20,
+                Width = 200,
+                Text = finalMessage
+            };
+            Button goToFolderButton = new Button
+            {
+                Text = "Go to folder",
+                Left = 50,
+                Width = 200,
+                Top = 50,
+                DialogResult = DialogResult.OK
+            };
+            if (count > 0) { 
+                
+                goToFolderButton.Click += (sender, e) =>
+                {
+                    System.Diagnostics.Process.Start("explorer.exe", folderPath);
+                };
+            }
+            else
+            {
+                goToFolderButton.Text = "Back";
+                goToFolderButton.Click += (sender, e) =>
+                {
+                    dialog.Close(); //close the pop up window
+                };
+            }
+            dialog.Controls.Add(label);
+            dialog.Controls.Add(goToFolderButton);
+            dialog.AcceptButton = goToFolderButton;
+
+            dialog.ShowDialog();
+        }
+        private bool ContainsAnyKeywords(string pdfFile, string[] keywords)
+        {
+            using (PdfDocument document = PdfDocument.Open(pdfFile))
+            {
+                foreach (Page page in document.GetPages()) //Page from PdfPig.Content
+                {
+                    var text = page.Text;
+                    using (StringReader reader = new StringReader(text))
+                    {
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            foreach (var keyword in keywords)
+                            {
+                                if (line.Contains(keyword.Trim(), StringComparison.OrdinalIgnoreCase))// letter case doesnt matter
+                                {
+                                    return true;
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+        private bool ContainsAllKeywords(string pdfFile, string[] keywords)
+        {
+            //map of wether the keywords was found
+            var keywordMap = keywords.ToDictionary(keyword => keyword, keyword => false);
+
+            using (PdfDocument document = PdfDocument.Open(pdfFile))
+            {
+                foreach (Page page in document.GetPages()) //Page from PdfPig.Content
+                {
+                    var text = page.Text;
+                    using (StringReader reader = new StringReader(text))
+                    {
+                        string line;
+                        var currentKeywords = keywordMap.Where(keyword => !keyword.Value).Select(keyword => keyword.Key).ToArray(); //or toList and then Remove
+                        while ((line = reader.ReadLine()) != null)
+                        {
+
+                            foreach (var keyword in currentKeywords)
+                            {
+                                //if the keyword has already been found then dont search for it
+                                if (line.Contains(keyword.Trim(), StringComparison.OrdinalIgnoreCase))// letter case doesnt matter
+                                {
+                                    keywordMap[keyword] = true;
+
+                                    //updating currentKeywords when there is change.
+                                    currentKeywords = keywordMap.Where(keyword => !keyword.Value).Select(keyword => keyword.Key).ToArray();
+                                }
+
+                            }
+
+                            if (!currentKeywords.Any()) //if there are none left to check then all have been found
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            return keywordMap.Values.All(found => found);//checks if all are true
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void checkBox_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
